5d5ed3347cc27f9d7280bec3dd16f6c7b38806e7
Lorena <lorena.perez@gmail.com>
2021-11-23 19:44:37 +0100

    no-bloking code

---
+++ 01-hola-mundo/app2.js
@@ -0,0 +1,4 @@
+const greeting = (name) => {
+  return `Hello ${name}`;
+};
+console.log(greeting("Lorena"));
---
+++ 01-hola-mundo/app3.js
@@ -0,0 +1,17 @@
+//ejemplo instrucciones no-blocking
+
+console.log("inicio de programa"); //1
+
+setTimeout(() => {
+  console.log("Primer Timeout"); //5
+}, 3000);
+
+setTimeout(() => {
+  console.log("Segundo Timeout"); //3
+}, 0);
+
+setTimeout(() => {
+  console.log("Tercer Timeout"); //4
+}, 0);
+
+console.log("fin de programa"); //2
