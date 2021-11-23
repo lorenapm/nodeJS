3007d69e3890e4e94382f9ca4c09c55281b58dae
Lorena <lorena.perez@gmail.com>
2021-11-23 21:34:12 +0100

    arrow function

---
+++ 02-JS/arrow.js
@@ -0,0 +1,16 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+//cuando arrow function tiene una sola línea y es el return, podemos resumirla
+//return implícito
+const sum = (a, b) => a + b;
+
+const greeting = () => "Hola mundo";
+
+console.log(sum(5, 10));
+console.log(greeting());
